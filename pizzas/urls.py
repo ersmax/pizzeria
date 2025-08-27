045d06d57fadcb3604f605ad2f026acc08b8a51a
ersmax <e.masarin@student.vu.nl>
2025-08-27 13:54:49 +0200

    Create homepage

---
+++ pizzas/urls.py
@@ -0,0 +1,9 @@
+"""Defines URL patterns for pizzas"""
+from django.urls import path        # build URL paths and map URLs to views
+from . import views                 # retrieved data and send it back with template
+
+app_name = 'pizzas'
+urlpatterns = [
+    # Homepage
+    path('', views.index, name='index'),
+]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """The home page for pizzas app"""
+    return render(request, 'pizzas/index.html')
